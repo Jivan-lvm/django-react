888d0b4d2988d3eba2ca136096cd7f751c5df6f4
Jivan-lvm <serezhkin.ivan@gmail.com>
2024-10-10 09:40:15 +0700

    add pages and navbar in front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,30 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './containers/Home'
+import Login from './containers/Login'
+import Signup from './containers/Signup'
+import Activate from './containers/Activate'
+import ResetPassword from './containers/ResetPassword'
+import ResetPasswordConfirm from './containers/ResetPasswordConfirm'
+import Layout from './hocs/Layout'
 
 const App = () => (
-  <Router>
-    <Switch>
-      
-    </Switch>
-  </Router>
-);
+	<Router>
+		<Layout>
+			<Routes>
+				<Route exact path='/' element={<Home />} />
+				<Route exact path='/login' element={<Login />} />
+				<Route exact path='/signup' element={<Signup />} />
+				<Route exact path='/activate/:token' element={<Activate />} />
+				<Route exact path='/resetpassword' element={<ResetPassword />} />
+				<Route
+					exact
+					path='/reset-password/:token'
+					element={<ResetPasswordConfirm />}
+				/>
+			</Routes>
+		</Layout>
+	</Router>
+)
 
-export default App;
+export default App
