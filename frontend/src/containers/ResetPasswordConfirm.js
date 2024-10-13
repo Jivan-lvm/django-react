a0f8b0bd3b66b1fa77fc0e9cccba1369803389a1
Jivan-lvm <serezhkin.ivan@gmail.com>
2024-10-13 21:22:10 +0700

    add reset password confirm

--- frontend/src/containers/ResetPasswordConfirm.js
+++ frontend/src/containers/ResetPasswordConfirm.js
@@ -1,8 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Navigate } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { reset_password_confirm } from '../actions/auth'
 
-const ResetPasswordConfirm = () => (
-    <div>
-        ResetPasswordConfirm
-    </div>)
+const ResetPasswordConfirm = ({ match, reset_password_confirm }) => {
+	const [requestSent, setRequestSent] = useState(false)
+	const [formData, setFormData] = useState({
+		new_password: '',
+        re_new_password: ''
+	})
 
-export default ResetPasswordConfirm
+	const { new_password, re_new_password } = formData
+
+	const onChange = e =>
+		setFormData({ ...formData, [e.target.name]: e.target.value })
+
+	const onSubmit = e => {
+		e.preventDefault()
+
+        const uid = match.uid
+        const token = match.token
+
+		reset_password_confirm(uid, token, new_password, re_new_password)
+		setRequestSent(true)
+	}
+
+	if (requestSent) {
+		return <Navigate to='/' />
+	}
+
+	return (
+		<div className='container mt-5'>
+			<form onSubmit={onSubmit}>
+				<div className='form-group'>
+					<input
+						className='form-control'
+						type='password'
+						placeholder='New password'
+						name='new_password'
+						value={new_password}
+						onChange={onChange}
+						required
+					/>
+				</div>
+				<div className='form-group'>
+					<input
+						className='form-control'
+						type='password'
+						placeholder='Confirm New Password'
+						name='re_new_password'
+						value={re_new_password}
+						onChange={onChange}
+						required
+					/>
+				</div>
+				<button className='btn btn-primary' type='submit'>
+					Сменить пароль
+				</button>
+			</form>
+		</div>
+	)
+}
+
+export default connect(null, { reset_password_confirm })(ResetPasswordConfirm)
