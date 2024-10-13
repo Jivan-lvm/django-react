1711341a5f83e2bb407ab3b859be1cef7980d26b
Jivan-lvm <serezhkin.ivan@gmail.com>
2024-10-13 16:49:18 +0700

    add navbar

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Home = () => (
-	<div>
-        Home
+	<div className='container'>
+		<div class='jumbotron mt-5'>
+			<h1 class='display-4'>Jivan's Birzha</h1>
+			<hr class='my-4' />
+			<p>Авторизация</p>
+			<Link class='btn btn-primary btn-lg'  to='/login' role='button'>
+				Login
+			</Link>
+		</div>
 	</div>
-);
+)
 
 export default Home;
